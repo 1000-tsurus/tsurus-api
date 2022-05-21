3df98d96ae8fcc5ce3a96cc58c37c3e7b55effa0
guilherme <guiliuri13@gmail.com>
2022-05-21 15:41:48 -0300

    fix for login query with soft_delte

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,22 +1,21 @@
-'use strict';
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import User from 'App/Models/User';
-import LoginValidator from 'App/Validators/LoginValidator';
+'use strict'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import LoginValidator from 'App/Validators/LoginValidator'
 
-export default class AuthController
-{
-    public async login ({ auth, request, response}: HttpContextContract)
-    {
-        const { email, password } = await request.validate(LoginValidator);
-        let user: User | null = null,
-            token: Record<string, any> | null = null;
+export default class AuthController {
+  public async login({ auth, request, response }: HttpContextContract) {
+    const { email, password } = await request.validate(LoginValidator)
+    let user: User | null = null,
+      token: Record<string, any> | null = null
 
-        if(email)
-        {
-            user = await User.findByOrFail('email', email);
-            token = await auth.use('api').attempt(email, password);
-        }
+    if (email) {
+      /* Verifica soft_delete */
+      user = await User.query().where('email', email).andWhereNull('deleted_at').firstOrFail()
 
-        return response.send({ user, token: token?.token });
+      token = await auth.use('api').attempt(email, password)
     }
+
+    return response.send({ user, token: token?.token })
+  }
 }
